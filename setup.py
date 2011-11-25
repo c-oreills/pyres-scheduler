987d0bea4d3a34231bfdd00668213a781654f059
Christy O'Reilly <christy@oreills.co.uk>
2011-11-25 15:06:26 +0000

    Add more metadata

--- setup.py
+++ setup.py
@@ -3,10 +3,23 @@ from distutils.core import setup
 setup(
     name='pyres_scheduler',
     version='0.1',
+    author='Christy O\'Reilly',
+    author_email='christy@oreills.co.uk',
+    maintainer='Christy O\'Reilly',
+    license='MIT',
+    url='http://github.com/c-oreills/pyres_scheduler',
     packages=['pyres_scheduler',],
     long_description=open('README.txt').read(),
     requires=[
         'APScheduler==2.0.2',
         'pyres==1.1',
-        ]
+        ],
+    classifiers = [
+        'Development Status :: 3 - Alpha',
+        'Environment :: Console',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python'
+        ],
 )
